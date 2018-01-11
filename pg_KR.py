83cc9007b36674367e6451780688aa06509229c4
krichards24 <33523437+krichards24@users.noreply.github.com>
2018-01-11 11:46:53 -0500

    Add files via upload

---
+++ pg_KR.py
@@ -0,0 +1,9 @@
+import pyautogui as pg
+
+pg.moveTo(1102,33,10)
+pg.click()
+pg.typewrite("yeezy.com\n",1)
+pg.moveTo(921, 135,1)
+pg.click()
+pg.hotkey("ctrl","t")
+pg.typewrite("AlexandreVauthier.com\n",1)
